63e7672cc98f9c34ba52463b2f8f5d92f0f433d4
Patrick Lafleur <plafleur25@gmail.com>
2018-01-24 21:55:45 +0000

    Added initial log in functions

--- src/books/core.clj
+++ src/books/core.clj
@@ -1,5 +1,26 @@
 (ns books.core
-  (:gen-class))
+    (:require [books.db :refer [db-string]]
+              [clojure.java.jdbc :as sql]
+              [buddy.hashers :as hashers]))
+
+(defn get-password [email]
+  (-> (sql/query db-string
+            ["select password from users where email = (?)" email]) first :password))
+
+(defn create-user! [email password]
+   (sql/insert! db-string :users
+                 {:email email 
+                  :password (hashers/derive password)}))
+                
+(defn log-in [email password]
+  (if (hashers/check password (get-password email))
+      "success"
+      "fail"))
+                
+(defn check-users []
+  (sql/query db-string
+             ["select * from users"]))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
