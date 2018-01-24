222e8c8dab0331fa22fd94862dd4cd55bf1eb4b8
Patrick Lafleur <plafleur25@gmail.com>
2018-01-24 03:29:45 +0000

    Updated project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject books "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Web app for users to track books read"
+  :url "https://github.com/plafleur/books"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
