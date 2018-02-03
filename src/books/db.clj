f71072b2cbf131a1eb134f00e0bea14cd42f2486
Patrick Lafleur <plafleur25@gmail.com>
2018-02-03 04:28:18 +0000

    Move DB functions to DB namespace

--- src/books/db.clj
+++ src/books/db.clj
@@ -1,7 +1,25 @@
-(ns books.db)
+(ns books.db
+    (:require
+        [clojure.java.jdbc :as sql]
+        [cemerick.friend :as friend]
+              (cemerick.friend [workflows :as workflows]
+                               [credentials :as creds])))
 
 (def db-string
   {:dbtype "postgresql"
  :dbname "books"
  :host "localhost"
  :port 5432})
+
+(defn get-user-info [req]
+    (let [user-info (first (sql/query db-string 
+                 ["select username, password, roles from users where username = (?)" req]))]
+            (cond
+            (= (:roles user-info) "user") (assoc user-info :roles #{:books.core/user})
+            (= (:roles user-info) "admin") (assoc user-info :roles #{:books.core/admin})
+            )))
+
+(defn create-user! [email password]
+   (sql/insert! db-string :users
+                 {:username email 
+                  :password (creds/hash-bcrypt password)}))
